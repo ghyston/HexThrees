bf6f18431094fbdc1e0c637949e3c55de238b991
Hyston <>
2018-08-24 07:11:16 +0200

    attempts to create motion blur shader with one pass

--- HexThrees Shared/GameScene.swift
+++ HexThrees Shared/GameScene.swift
@@ -10,6 +10,9 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var selfRenderTextureNode = SKSpriteNode()
+    var cropRect : CGRect?
+    
     class func newGameScene() -> GameScene {
         
         guard let scene = SKScene(fileNamed: "GameScene") as? GameScene else {
@@ -19,10 +22,31 @@ class GameScene: SKScene {
         
         scene.scaleMode = .resizeFill
         
+        scene.addChild(scene.setupEffectNoteForTest())
+        
+        return scene
+    }
+    
+    func setupEffectNoteForTest() -> SKNode {
+        
+        let count : Float = 10.0
+        let sum : Float = count * (count + 1.0) / 2.0
+        let speed : Float = 0.5
+        
         let motionBlurHolder = SKEffectNode.init();
         let fShader = SKShader.init(fileNamed: "blur.fsh")
         let sprite = SKSpriteNode.init(imageNamed: "fucking_cat")
         let circle = SKShapeNode.init(circleOfRadius: 150)
+        
+        let countUniform = SKUniform.init(name: "COUNT", float: count);
+        let sumUniform = SKUniform.init(name: "SUM", float: sum);
+        fShader.addUniform(countUniform);
+        fShader.addUniform(sumUniform);
+        
+        let speedAttr = SKAttribute.init(
+            name: "SPEED", type: .float)
+        fShader.attributes.append(speedAttr)
+        motionBlurHolder.setValue(SKAttributeValue(float: speed), forAttribute: "SPEED");
         
         circle.fillColor = .red
         motionBlurHolder.shader = fShader
@@ -30,16 +54,20 @@ class GameScene: SKScene {
         
         motionBlurHolder.addChild(circle)
         motionBlurHolder.addChild(sprite)
-        scene.addChild(motionBlurHolder)
-        
-        return scene
+        return motionBlurHolder
     }
     
     override func didMove(to view: SKView) {
         
+        self.selfRenderTextureNode.size = view.frame.size
+        
+        cropRect = CGRect(x: -400, y: -300, width: 800, height: 600)
     }
     
     override func update(_ currentTime: TimeInterval) {
+        
+        let sceneTexture = self.scene.view?.texture(from: rootNode, crop: cropRect!)
+        
         
     }
 }
--- HexThrees Shared/Shaders/blur.fsh
+++ HexThrees Shared/Shaders/blur.fsh
@@ -1,18 +1,14 @@
 void main()
 {
-    float step = 0.05;
-    float count = 5.0;
+    float step = fract(u_time * SPEED) * 0.05;
     vec4 result = vec4(0.0);
     
-    // s(n) = (n * (n + 1)) / 2
-    float sum = count * (count + 1.0) / 2.0;
-    
-    for (float i = -count; i < count; i++)
+    for (float i = -COUNT; i < COUNT; i++)
     {
         vec2 tex_coord = v_tex_coord;
         tex_coord.x += i * step;
         vec4 sample = texture2D(u_texture, tex_coord);
-        float coeff = (count - abs(i)) / sum;
+        float coeff = (COUNT - abs(i)) / SUM;
         
         result += sample * coeff;
     }
