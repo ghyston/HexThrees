cf05c4db6f5ad8f52c24b69cce4d623bedd092ba
Hyston <>
2019-09-09 09:16:47 +0200

    Refactorings: * adding new cell in separate command * add finish callback to playback * remove calling to stressTimerNode
    New:
    * use timer shader animation and predefined cell to be blocked

---
+++ HexThrees Shared/Commands/AddCellByTimerCMD.swift
@@ -0,0 +1,9 @@
+//
+//  AddCellByTimerCMD.swift
+//  HexThrees iOS
+//
+//  Created by Ilja Stepanow on 09.09.19.
+//  Copyright © 2019 Ilja Stepanow. All rights reserved.
+//
+
+import Foundation
---
+++ HexThrees Shared/Commands/Timer/StartStressTimerCMD.swift
@@ -0,0 +1,42 @@
+//
+//  StartTimerCMD.swift
+//  HexThrees iOS
+//
+//  Created by Ilja Stepanow on 09.05.19.
+//  Copyright © 2019 Ilja Stepanow. All rights reserved.
+//
+
+import Foundation
+
+class StartStressTimerCMD : GameCMD {
+    
+    override func run() {
+        
+        guard gameModel.stressTimer.isEnabled() else {
+            return
+        }
+        
+        var cells = self.gameModel.field.getBgCells(compare: HexField.freeCellWoBonuses)
+        if cells.count == 0 {
+            cells = self.gameModel.field.getBgCells(compare: HexField.freeCell)
+        }
+        
+        guard let bgCell = cells.randomElement() else {
+            return
+        }
+        
+        bgCell.startCircleAnimation()
+        
+        
+        self.gameModel.stressTimer.startNew(
+            timer:  AddRandomCellCMD(self.gameModel)
+                .runWithDelay(delay: GameConstants.StressTimerInterval),
+            cell: bgCell)
+        
+        
+        //@todo: remove this notification type
+        /*NotificationCenter.default.post(
+            name: .startTimer,
+            object: nil)*/
+    }
+}
