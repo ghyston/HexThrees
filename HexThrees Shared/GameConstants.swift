19657b491a2fb57bae0b719c99e12c2494e88232
Hyston <ghyston@gmail.com>
2020-05-08 14:14:03 +0200

    Tweak probability constants

--- HexThrees Shared/GameConstants.swift
+++ HexThrees Shared/GameConstants.swift
@@ -28,14 +28,14 @@ class GameConstants {
 	
 	// Probabilities
 	static let RandomCellIsValue2Probability: Float = 0.3
-	static let BaseBonusDropProbability =  10.1 //@todo: 0.1
+	static let BaseBonusDropProbability = 0.2
 	static let LockBonusProbability: Float =  0.5
 	static let UnlockBonusProbability: Float = 0.3
 	static let LastBlockedUnlockBonusProbability: Float = 0.05
 	static let X2BonusProbability: Float = 0.2
 	static let X3BonusProbability: Float = 0.1
-	static let ExpandFieldOriginalProbability: Float = 10.0 //@todo: 0.5?
-	static let ExpandFieldDropProbability: Float = 0.1 //@todo: 0.2?
+	static let ExpandFieldOriginalProbability: Float = 0.7
+	static let ExpandFieldDropProbability: Float = 0.25
 	
 	static let CollectableUnlockCellBonusProbability: Float = 1.0
 	static let CollectableSwipeBlockBonusProbability: Float = 1.0
